3594d95c4833e048d4c00f732c491b4d266de29e
Carlos Gómez Huélamo <cram3r95@gmail.com>
2020-10-27 09:14:28 +0100

    Update launch_docker_image.sh

--- scripts/launch_docker_image.sh
+++ scripts/launch_docker_image.sh
@@ -4,7 +4,7 @@
 
 # File to launch a docker image using a specific image (first argument), with a certain container name (second arg.), user (third arg.) and number of tabs (fourth arg.).
 
-# Example: ./launch_docker_image.sh my_docker_image:last my_container_name my_user 4 (four tabs)
+# Example: ./launch_docker_image.sh my_docker_image:last my_container_name my_user 4
 
 # N.B. If this user was not previously included in the docker image, first enter as root and include this user:
 
@@ -17,14 +17,19 @@
 
         # Now you can use your new user (with its corresponding home directory)
 
+# Note: Do not include empty volumes in the docker run command. In that case (e.g. aux1="", -v $aux1), it will generate the following error: 
+
+#        docker: invalid reference format: repository name must be lowercas 
+
 # 1. Set named volumes
 
-aux1=""
+shared=""
 
 if [[ $3 != "root" ]]; # Non-root user
 then
 	shared=$HOME/shared_home:/home/$3/shared_home
 else                   # Root user
+        
 	shared=$HOME/shared_home:/$3/shared_home
 fi
 
@@ -33,16 +38,26 @@ then
 	aux1=/media/robesafe/Data_RobeSafe/2D_MOT:/home/robesafe/perception_githubs/deepmot/test_tracktor/2D_MOT	
 fi
 
+if [[ $1 == "uahrobesafe/carla-challenge:last" ]]; # CARLA challenge image
+then
+        shared=$HOME/shared_home:/workspace/shared_home
+	aux1=$HOME/robesafe_code:/workspace/team_code	
+fi
+
 # 2. Kill previous container 
 
 docker stop $2 
 docker rm -fv $2
 
+echo "Image name: " $1
+echo "Container name: " $2
+echo "User: " $3
+echo "Number of tabs: " $4
+
 # 3. Run container 
 
 if [[ $4 -gt 0 ]]; # Multiple tabs
 then
-        echo "Multiple tabs"
 	for (( i=1; i<=$4; i++ ))
 	do 
 		command=""
@@ -56,10 +71,8 @@ then
 		gnome-terminal --tab "$i" -e "$command"
 	done
 else 	           # Single tab
-        echo "Single tab"
 	docker run -it --net host --name=$2 --privileged -u $3 -v /tmp/.X11-unix:/tmp/.X11-unix -v $shared -e DISPLAY=unix$DISPLAY $1 /bin/bash
 fi
 
 # -v $aux1
-
 # --runtime=nvidia
