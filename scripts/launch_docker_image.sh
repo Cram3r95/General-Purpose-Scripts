9b650e3f539f440a60b5560f720b1da4964865b9
Carlos Gómez Huélamo <cram3r95@gmail.com>
2020-12-14 14:57:02 +0100

    Update launch_docker_image.sh

--- scripts/launch_docker_image.sh
+++ scripts/launch_docker_image.sh
@@ -2,9 +2,15 @@
 
 # Carlos Gómez-Huélamo - September 2020
 
-# File to launch a docker image using a specific image (first argument), with a certain container name (second arg.), user (third arg.) and number of tabs (fourth arg.).
+# File to launch a docker image. Arguments:
 
-# Example: ./launch_docker_image.sh my_docker_image:last my_container_name my_user 4
+# 1. Image name
+# 2. Container name
+# 3. User's name
+# 4. Number of tabs
+# 5. Force to kill the container associated to that image (if exists) and create a new one (true/false)
+
+# Example: ./launch_docker_image.sh my_docker_image:last my_container_name my_user 4 false
 
 # N.B. If this user was not previously included in the docker image, first enter as root and include this user:
 
@@ -17,62 +23,109 @@
 
         # Now you can use your new user (with its corresponding home directory)
 
-# Note: Do not include empty volumes in the docker run command. In that case (e.g. aux1="", -v $aux1), it will generate the following error: 
+# Function to create a new container and run multiple tabs
 
-#        docker: invalid reference format: repository name must be lowercas 
+create_new_container()
+{
+	# Kill previous container
+
+	docker stop $2
+	docker rm -fv $2
+
+	# Create and run multiple tabs
+
+	if [[ $4 -gt 0 ]]; # Multiple tabs
+	then
+		for (( i=1; i<=$4; i++ ))
+		do 
+			command=""
+			if [[ $i -eq 1 ]]; 
+			then 
+				command="docker run -it --net host --name=$2 --privileged -u $3 -v /tmp/.X11-unix:/tmp/.X11-unix -v $shared -v $linkedin_courses -e DISPLAY=unix$DISPLAY $1 /bin/bash"
+			else
+				command="bash -c 'docker exec -it $2 /bin/bash'"
+			fi
+
+			gnome-terminal --tab "$i" -e "$command"
+		done
+	else 	           # Single tab
+		docker run -it --net host --name=$2 --privileged -u $3 -v $shared -v $linkedin_courses -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY $1 /bin/bash
+	fi
+}
+
+# Function to restart a stopped container and run multiple tabs
+
+restart_container()
+{
+	if [[ $4 -gt 0 ]]; # Multiple tabs
+	then
+		docker start $2
+
+		for (( i=1; i<=$4; i++ ))
+		do 
+			command="bash -c 'docker exec -it $2 bash'"
+
+			gnome-terminal --tab "$i" -e "$command"
+		done
+	else 	           # Single tab
+		docker start $2 && docker exec -it $2 bash
+	fi
+}
 
 # 1. Set named volumes
-
-shared=""
 
 if [[ $3 != "root" ]]; # Non-root user
 then
 	shared=$HOME/shared_home:/home/$3/shared_home
+	linkedin_courses=$HOME/Desktop/LinkedIn_Courses:/home/$3/LinkedIn_Courses
 else                   # Root user
-        
 	shared=$HOME/shared_home:/$3/shared_home
+    linkedin_courses=$HOME/Desktop/LinkedIn_Courses:/$3/LinkedIn_Courses
 fi
 
-if [[ $1 == "perception_githubs:last" ]]; # Perception GitHubs image
-then
-	aux1=/media/robesafe/Data_RobeSafe/2D_MOT:/home/robesafe/perception_githubs/deepmot/test_tracktor/2D_MOT	
-fi
+# 2. Check status of the container
 
-if [[ $1 == "uahrobesafe/carla-challenge:last" ]]; # CARLA challenge image
-then
-        shared=$HOME/shared_home:/workspace/shared_home
-	aux1=$HOME/robesafe_code:/workspace/team_code	
-fi
-
-# 2. Kill previous container 
-
-docker stop $2 
-docker rm -fv $2
+check_running=$(docker ps --filter "name=$2" -q)
+check_all=$(docker ps -a --filter "name=$2" -q)
 
 echo "Image name: " $1
 echo "Container name: " $2
 echo "User: " $3
 echo "Number of tabs: " $4
 
-# 3. Run container 
+# 3. Run the image
 
-if [[ $4 -gt 0 ]]; # Multiple tabs
+if [[ -z $check_all ]]; 
 then
-	for (( i=1; i<=$4; i++ ))
-	do 
-		command=""
-		if [[ $i -eq 1 ]]; 
-		then 
-			command="docker run -it --net host --name=$2 --privileged -u $3 -v /tmp/.X11-unix:/tmp/.X11-unix -v $shared -e DISPLAY=unix$DISPLAY $1 /bin/bash"
-		else
-			command="bash -c 'docker exec -it $2 /bin/bash'"
-		fi
+	echo $'\nThe container does not exist'
 
-		gnome-terminal --tab "$i" -e "$command"
-	done
-else 	           # Single tab
-	docker run -it --net host --name=$2 --privileged -u $3 -v /tmp/.X11-unix:/tmp/.X11-unix -v $shared -e DISPLAY=unix$DISPLAY $1 /bin/bash
+	# 3.1. Create new container 
+
+	create_new_container
+else
+	# 3.2. Check if your container is stopped or it is currently running
+
+	if [[ -z "$check_running" ]];
+	then
+		echo $'\nThe container is stopped. Restart and run multiple tabs or create a new one'
+
+		if [[ $5 == "true" ]];
+		then
+			echo "Create a new container"
+			create_new_container $1 $2 $3 $4
+		else
+			echo "Restart the container"
+			restart_container $1 $2 $3 $4
+		fi
+	else	
+		echo $'\nThe container is currently running'
+	
+		if [[ $5 == "true" ]];
+		then
+			echo "Create a new container"
+			create_new_container $1 $2 $3 $4
+		fi
+	fi
 fi
 
-# -v $aux1
 # --runtime=nvidia
